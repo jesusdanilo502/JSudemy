e634cc2eb267bcfd8ab8acc51f48395778d2daf0
onemas <jesusdanilo502@users.noreply.github.com>
2018-01-18 21:31:07 -0500

    archivo js

---
+++ variables.js
@@ -0,0 +1,9 @@
+var palabra ="palabra";
+console.log("palabra", palabra);
+
+var boleana = true;
+console.log("boleana",boleana);
+
+// variable de tipo arreglo
+var colores= ["rojo","amarillo"]
+console.log("colores",colores[1]);
