74b7a18132141ac6d96039cc97ca94ed8706b094
onemas <jesusdanilo502@users.noreply.github.com>
2018-02-13 22:18:48 -0500

    sección 2
    se trabaja en la sección dos del curso de js

--- variables.js
+++ variables.js
@@ -7,3 +7,18 @@ console.log("boleana",boleana);
 // variable de tipo arreglo
 var colores= ["rojo","amarillo"]
 console.log("colores",colores[1]);
+
+var cars= ["saab","volvo", "bmw"];
+// objetos
+var person = {firstName:"jonh", lastName:"Doe", age:50 ,eyeColor:"Blue"};
+
+
+var result =1;
+var counter = 0;
+while (counter <10){
+    result= result*2;
+    counter= counter+1;
+}
+console.log(result);
+
+
