79ce18c6386b40f0690a15b3575e237b1ba637a5
Danilo <serviciosonemas@gmail.com>
2018-07-25 22:42:43 -0500

    Propiedades
    se agregan propiedades y metodos para los botones

--- Calculadora/js/codigo.js
+++ Calculadora/js/codigo.js
@@ -4,37 +4,62 @@
 
 var p = {
     teclas: document.querySelectorAll("#calculadora ul li"),
-    accion: null
+    accion: null,
+    digito: null,
+    operaciones: document.querySelector("#operaciones"),
+    cantidadSignos: 0
 }
 /* OBJETOS CON LOS METODOS DE LA CALCULADORA */
 
-var m= {
-  inicio: function () {
-      for ( var i=0; i < p.teclas.length; i++){
-          p.teclas[i].addEventListener("click", m.oprimirTecla)
-      }
+var m = {
+    inicio: function () {
+        for (var i = 0; i < p.teclas.length; i++) {
+            p.teclas[i].addEventListener("click", m.oprimirTecla)
+        }
 
     },
     oprimirTecla: function (tecla) {
-        console.log(tecla.target.getAttribute("class"));
-        m.calculadora(p.accion);
-    },
-    calculadora: function (accion) {
-      switch (accion){
-          case "numero":
-              console.log("numero");
-              break;
-          case "signo":
-              console.log("signo");
-              break;
-          case "decimal":
-              console.log("decimal");
-              break;
-          case "igual":
-              console.log("igual");
-              break;
-      }
+        p.accion = tecla.target.getAttribute("class");
+        p.digito = tecla.target.innerHTML;
 
+        m.calculadora(p.accion, p.digito);
+    },
+    calculadora: function (accion, digito) {
+        switch (accion) {
+            case "numero":
+                p.cantidadSignos = 0;
+                if (p.operaciones.innerHTML == 0) {
+                    p.operaciones.innerHTML = digito;
+                }
+                else {
+                    p.operaciones.innerHTML += digito;
+                }
+
+                break;
+            case "signo":
+                p.cantidadSignos++
+                if (p.cantidadSignos == 1) {
+                    if (p.operaciones.innerHTML == 0) {
+                        p.operaciones.innerHTML = 0;
+                    }
+                    else {
+                        p.operaciones.innerHTML += digito;
+                    }
+
+                }
+
+                break;
+            case "decimal":
+                console.log("decimal");
+                break;
+            case "igual":
+                console.log("igual");
+                break;
+        }
+
+    },
+    borrarCalculadora: function () {
+        p.operaciones.innerHTML = 0;
     }
 }
 m.inicio();
