6786a274b56bf98200467310922386e6e4b27012
Danilo <serviciosonemas@gmail.com>
2018-07-31 22:03:38 -0500

    creacion de efecto de paginacion slideshow

--- SlideShow/js/slide.js
+++ SlideShow/js/slide.js
@@ -5,7 +5,10 @@ OBJETO CON LAS PROPIEDADES DEL SLIDE
 var p = {
 
     paginacion: document.querySelectorAll("#paginacion li"),
-    item: 0
+    item: 0,
+    cajaSlide: document.querySelector("#slide ul"),
+    animacionSlide: "fade",
+    imgSlide: document.querySelectorAll("#slide ul li")
 
 }
 
@@ -24,12 +27,32 @@ var m = {
     },
     paginacionSlide: function (item) {
 
-        p.item = item.target.parentNode.getAttribute("item")-1;
+        p.item = item.target.parentNode.getAttribute("item") - 1;
         m.movimientoSlide(p.item);
     },
-    movimientoSlide: function(item) {
-        console.log("item", -((item * 100) + "%"));
+    movimientoSlide: function (item) {
 
+        p.cajaSlide.style.left = item * -100 + "%";
+
+        for (var i = 0; i < p.paginacion.length; i++) {
+            p.paginacion[i].style.opacity = .5;
+        }
+        p.paginacion[item].style.opacity = 1;
+
+        if (p.animacionSlide == "slide") {
+            p.cajaSlide.style.transition = ".7s left ease-in-out";
+        }
+        if (p.animacionSlide == "fade") {
+
+            p.imgSlide[item].style.opacity = 0;
+            p.imgSlide[item].style.transition = " .7s opacity ease-in-out";
+            setTimeout(function () {
+
+                p.imgSlide[item].style.opacity = 1;
+            }, 500)
+
+
+        }
     }
 }
 
